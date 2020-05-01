a8e7cdbad7ad4be5707961bb47b55e9e25453724
Simon Anders <sanders@fs.tum.de>
2020-05-01 17:27:22 +0200

    minor

--- Figure_3.R
+++ Figure_3.R
@@ -26,17 +26,17 @@ dev.off()
 ## Figure 3c
 
 tecan %>% 
-filter( plate %in% c( "CP00001" ), gene=="N", plateRemark=="2", minutes == 30 ) %>%
+filter( plate == "CP00001", minutes > 30 ) %>%
 left_join( tblCT ) %>%
+filter( !is.na(CT) ) %>%
 mutate( CT = ifelse( CT>40, runif( n(), 43, 46 ), CT ) ) %>%
 ggplot +
   geom_vline( xintercept = 41.5, col="darkgray" ) +
-  geom_point( aes( x=CT, y=absBlue-absYellow ) ) +
+  geom_point( aes( x=CT, y=absBlue-absYellow, col=gene ) ) +
   theme_bw() + theme( panel.grid.major = element_blank(), panel.grid.minor = element_blank() ) + 
   scale_x_continuous( breaks = c( 20, 30, 40, 44.5 ), labels = c( 20, 30, 40, "neg" ) ) +
   xlab( "CT value from qPCR" ) + 
-  ylab( expression( OD["434 nm"] -  OD["560 nm"] ) ) +
-  ggtitle( "30 min, gene N" )
+  ylab( expression( OD["434 nm"] -  OD["560 nm"] ) ) 
 
 dev.copy( svg, "figs/Figure_3c.svg", width=3.8, height=2.5 )
 dev.off()
--- load_sheets.R
+++ load_sheets.R
@@ -7,8 +7,9 @@ read_tsv( "data/tecan_sheets.tsv" ) -> sheet_list
 map_dfr( function(i) read_tecan( 
   file.path( "data/tecan_scans", sheet_list$filename[i] ), 
   sheet_list$sheetname[i] ) ) %>%
-left_join( sheet_list ) %>%
-select( plate, gene, heat, minutes, plateRemark=remark, well, 
+left_join( sheet_list ) %>%  
+mutate( heat95 = heat==95 ) %>%
+select( plate, gene, heat95, minutes, plateRemark=remark, well, 
   absBlue=blue_absorbance, absYellow = yellow_absorbance ) -> tecan
 
 tecan %>% write_tsv( "data/tecan_values.tsv" ) 
