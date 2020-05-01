1782fce90ec2be087d33e5053eb33c761aee0981
herbstk <k.herbst@stud.uni-heidelberg.de>
2020-05-01 19:27:27 +0200

    finalized Fig 5

--- Figure_5.R
+++ Figure_5.R
@@ -13,8 +13,6 @@ lamp_thresholds <- c(-.05, .3)
 qpcr_thresholds <- c(30, 42)
 ngs_threshold <- 3000
 
-
-
 ## Figure 5B
 tbl <- ngs %>%
   filter( !is.na(matchedTRUE)) %>%
@@ -24,29 +22,31 @@ tbl <- ngs %>%
   filter( ! ( plate == "CP00005" & well >= "G" ) ) %>%
   left_join( tblCT )%>%
   filter( !is.na(CT) ) %>%
-  mutate( CT = ifelse( CT>40, runif( n(), 43, 46 ), CT ) ) %>%
+  mutate( CT = ifelse( CT>40, runif( n(), 43, 47 ), CT ) ) %>%
   mutate( NGS = case_when(
     matchedTRUE > ngs_threshold ~ "positive",
     matchedTRUE <= ngs_threshold ~ "negative",
     TRUE                ~ "undetected")) %>%
   mutate_at( "NGS", fct_relevel, "positive", "negative" )
+
 ggplot(tbl) +
   geom_hline(yintercept = lamp_thresholds, color = "lightgray" ) +
-  geom_vline(xintercept = qpcr_thresholds[[1]], color = "lightgray" ) +
-  geom_vline(xintercept = qpcr_thresholds[[2]], color = "grey20" ) +
-  geom_point( aes( x = CT, y = absBlue - absYellow, fill = NGS ), colour = "black", alpha = .6, shape = 21, size = 2 ) + 
-  #scale_x_continuous( breaks = c( 20, 30, 40, 45 ), labels = c( 20, 30, 40, "neg" ) ) +
-  scale_x_continuous( breaks = c( 20, 30, 40, 45 ), labels = c( 20, 30, 40, "neg" ), trans = "reverse" ) +
-  labs(subtitle = str_interp( "${nrow(tbl)} samples on ${tbl%>%select(plate)%>%unique%>%nrow} plates" ),
-       x = expression(paste("RT-qPCR (CT value)")),
-       y = expression(paste("RT-LAMP assay (", Delta, OD, ")"))) +
+  geom_vline(xintercept = qpcr_thresholds, color = "darkgrey" ) +
+  geom_point( aes( x = CT, y = absBlue - absYellow, fill = NGS ), colour = "black", alpha = .6, shape = 21, size = 1.2 ) + 
+  scale_x_continuous( breaks = c( 20, 30, 40, 45 ), labels = c( 20, 30, 40, "neg" ) ) +
+  #scale_x_continuous( breaks = c( 20, 30, 40, 45 ), labels = c( 20, 30, 40, "neg" ), trans = "reverse" ) +
+  labs(title = str_interp( "30 min at 65°C\n${nrow(tbl)} samples on ${tbl%>%select(plate)%>%unique%>%nrow} plates" ),
+       x = "RT-qPCR (CT value)",
+       y = "RT-LAMP assay (ΔOD)") +
   scale_fill_manual(values = c("positive" = "black", "negative" = "white")) +
   #facet_grid(cols = vars(minutes), labeller = as_labeller(function(x) str_c(x, " min at 65°C"))) +
-  theme_bw(base_size = 16) +
+  theme_bw() +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), strip.background = element_blank()) +
-  annotate("text", x = 11, y = -.30, label = str_glue("negative"), angle = 90) +
-  annotate("text", x = 11, y = .06, label = str_glue("inconclusive"), angle = 90) +
-  annotate("text", x = 11, y = .4, label = str_glue("positive"), angle = 90)
+  annotate("text", x = 11.25, y = -.26, label = str_glue("negative"), angle = 90) +
+  annotate("text", x = 11.25, y = .125, label = str_glue("inconclusive"), angle = 90) +
+  annotate("text", x = 11.25, y = .425, label = str_glue("positive"), angle = 90) +
+  coord_cartesian(xlim = c(11.75, 47.5))
 
-dev.copy( svg, "figs/Figure_5b.svg", width=8, height=6 )
-dev.off()
+# Export figures
+ggsave("figs/Figure_5b.svg", width=14, height=10, units="cm")
+ggsave("figs/Figure_5b.png", width=14, height=10, units="cm", dpi=300)
