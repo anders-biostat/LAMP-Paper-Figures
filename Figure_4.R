6baa16b0ae836ea8545765a3b64563b9310d4f2b
Simon Anders <sanders@fs.tum.de>
2020-05-01 21:45:06 +0200

    Fig 4

--- Figure_4.R
+++ Figure_4.R
@@ -5,7 +5,7 @@ library(glue)
 library(ggsci)
 library(binom)
 
-read_tsv( "data/tecan_values.tsv" ) -> tecan
+read_tsv( "data/tecan_values.tsv", col_types = "ccliccdd" ) -> tecan
 read_tsv( "data/plates_with_CTs.tsv" ) -> tblCT
 
 plates_to_use <- c( "CP00001", "CP00003", "CP00005", "CP00006", "CP00008", 
@@ -21,7 +21,8 @@ tecan %>%
   left_join( tblCT ) %>% 
   filter( !is.na(CT) ) -> tbl
 
-fig4a <- tbl %>%
+fig4a <- 
+tbl %>%
   mutate(CT = ifelse(CT > 40, runif(n(), 43, 47), CT)) %>%
   mutate(plate = str_extract(plate, "\\d{2}$")) %>%
   sample_frac() %>%  # shuffle points to randomize overplotting at least
@@ -29,36 +30,42 @@ fig4a <- tbl %>%
   geom_hline(yintercept = lamp_thresh, color = "lightgray" ) +
   geom_vline(xintercept = c(30, 41.5), color = "darkgray" ) +
   geom_point(aes(x = CT, y = absBlue - absYellow, color = plate), size = .7) +
-  scale_x_continuous(breaks = c(20, 30, 40, 45), labels = c(20, 30, 40, "negative")) +
+  scale_x_reverse(breaks = c(20, 30, 40, 45), labels = c(20, 30, 40, "negative")) +
   scale_color_d3(palette="category20") +
-  annotate("text", x = 11.25, y = -.26, label = glue("negative"), angle = 90) +
-  annotate("text", x = 11.25, y = .125, label = glue("inconclusive"), angle = 90) +
-  annotate("text", x = 11.25, y = .425, label = glue("positive"), angle = 90) +
-  coord_cartesian(xlim = c(11.75, 47.5)) +
+  annotate("text", x = 49, y = -.26, label = glue("negative"), 
+    angle = 90, col="darkgray") +
+  annotate("text", x = 49, y = .125, label = glue("inconclusive"), 
+    angle = 90, col="darkgray") +
+  annotate("text", x = 49, y = .425, label = glue("positive"), 
+    angle = 90, col="darkgray") +
+  coord_cartesian(xlim = c(48.5, 11.75 )) +
   theme_bw() +
   theme(panel.grid.major = element_blank()) +
   labs(title = "a",
        subtitle = glue("30 min at 65°C\n{nrow(tbl)} samples on {tbl%>%distinct(plate)%>%nrow} plates"),
        x = glue("RT-qPCR (CT value)"),
        y = "RT-LAMP assay (ΔOD)")
-
+fig4a
 
 
 ## Figure 4b
 ct_breaks <- c(0, 25, 30, 35, 40, Inf)
-ct_binlabels <- c("0-25", "26-30", "31-35", "36-40")
+ct_binlabels <- c("0-25", "25-30", "30-35", "35-40")
 
 # classify samples into false positives, TP, FN, TN...
 lamp_cls <- tbl %>%
   mutate(delta_abs = absBlue - absYellow,
-         lamp_result = if_else(delta_abs > lamp_thresh[1], "positive", "negative"),
+         lamp_result = case_when(delta_abs > lamp_thresh[2] ~ "positive", 
+                                 delta_abs < lamp_thresh[1] ~ "negative",
+                                 TRUE                       ~ "inconclusive" ),
          qpcr_result = if_else(CT == Inf, "negative", "positive")) %>%
   mutate(result = case_when(lamp_result == "negative" & qpcr_result == "negative" ~ "TN",
                             lamp_result == "positive" & qpcr_result == "positive" ~ "TP",
                             lamp_result == "positive" & qpcr_result == "negative" ~ "FP",
                             lamp_result == "negative" & qpcr_result == "positive" ~ "FN"))
 
-ss_binned <- lamp_cls %>%
+ss_binned <- 
+lamp_cls %>%
   mutate(ct_bin = cut(CT, ct_breaks)) %>%
   group_by(result, ct_bin) %>%
   tally() %>% ungroup() %>%
@@ -96,12 +103,14 @@ fig4a + fig4b + plot_layout(widths = c(10, 8)) &
   theme(plot.title = element_textbox_simple(face="bold"), panel.grid.minor = element_blank())
 
 # Export figures
-ggsave("Figure_4.svg", width=20, height=10, units="cm")
-ggsave("Figure_4.png", width=20, height=10, units="cm", dpi=300)
+ggsave("figs/Figure_4.svg", width=20, height=10, units="cm")
+ggsave("figs/Figure_4.png", width=20, height=10, units="cm", dpi=300)
 
 
 
 ## Confusion matrix
+## Alernative code, for manual comparison
+
 tbl %>%
 mutate( CTbin = cut( CT, ct_breaks ) ) %>% 
 mutate_at( "CTbin", recode, 	"neg" = "(40,Inf]" ) %>%
@@ -111,5 +120,8 @@ mutate( LAMPres =
    { c( "neg", "incl", "pos" )[.] } ) %>%
 group_by( CTbin, LAMPres ) %>%
 count() %>%
-pivot_wider( names_from = LAMPres, values_from = n, values_fill = c(n=0) ) 
+pivot_wider( names_from = LAMPres, values_from = n, values_fill = c(n=0) )  ->
+  confusion_matrix
+
+bind_cols( confusion_matrix, ss_binned ) %>% write_tsv( "LAMP_confMatrix.tsv" )
 
