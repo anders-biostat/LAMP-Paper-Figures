c813bff42079963e781323ec7ef3086680bb4266
Lukas <l.kremer@dkfz.de>
2020-05-02 14:48:23 +0200

    made Fig4 look more like the other figures

--- Figure_3.R
+++ Figure_3.R
@@ -13,12 +13,12 @@ mutate( CT = ifelse( CT>40, runif( n(), 43, 46 ), CT ) ) %>%
 filter( !is.na(CT) ) %>%
 filter( plate %in% "CP00001", gene=="N", plateRemark=="2" ) %>% 
 ggplot +
-  geom_line( aes( x=minutes, y=absBlue-absYellow, group=well, col=CT ) ) +
+  geom_line( aes( x=minutes, y=absBlue-absYellow, group=well, col=CT ), size=.5 ) +
   scale_color_ct() +
   xlab( "incubation time [minutes]" ) + 
   ylab( expression( "LAMP signal (ΔOD)" ) ) -> plot3b
 
-print(plot3b)
+plot3b
 
 ## Figure 3cd
 
@@ -27,17 +27,19 @@ filter( plate == "CP00001" ) %>%
 filter( ( gene=="N" & minutes == 30 ) | ( gene=="1a" & minutes==40 ) ) %>%
 left_join( tblCT ) %>%
 filter( !is.na(CT) ) %>%
-mutate( CT = ifelse( CT>40, runif( n(), 43, 46 ), CT ) ) %>%
-mutate( facet = str_c( "gene ", gene, ", ", minutes, " min" ) ) %>%
+mutate( CT = ifelse( CT>40, runif( n(), 43, 47 ), CT ) ) %>%
+mutate( facet = str_c( gene, " gene, ", minutes, " min" ) ) %>%
 mutate_at( vars(facet), fct_rev ) %>%
 ggplot +
-  geom_vline( xintercept = 41.5, col="darkgray" ) +
+  geom_vline( xintercept = 42, col="darkgray" ) +
   geom_point( aes( x=CT, y=absBlue-absYellow ), size=.7, shape=19, alpha=.6, fill="lightgray" ) +
-  scale_x_reverse( breaks = c( 20, 30, 40, 44.5 ), labels = c( 20, 30, 40, "neg" ) ) +
+  scale_x_reverse( breaks = c( 10, 20, 30, 40, 45 ), labels = c( 10, 20, 30, 40, "neg" ) ) +
   xlab( "RT-qPCT (CT value)" ) + 
   ylab( "RT-LAMP (ΔOD)" ) +
   facet_wrap( . ~ facet ) -> plot3c
 
-( plot_spacer() | plot3b ) / plot3c
-dev.copy( svg, "Figure_3.svg", width=9, height=6 )
-dev.off()
+( ggplot() | plot3b ) / plot3c +
+  plot_annotation(tag_levels = "a")
+
+ggsave("SVGs/Figure_3.svg", width=20, height=14, units="cm")
+ggsave("Figure_3.png", width=20, height=14, units="cm", dpi=300)
