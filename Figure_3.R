99e48049bc1446a7414a0e2493aa66b8f8a520cd
Lukas <l.kremer@dkfz.de>
2020-05-02 19:42:17 +0200

    split Fig3 into Fig3 and SFig3

--- Figure_3.R
+++ Figure_3.R
@@ -20,26 +20,42 @@ ggplot +
 
 plot3b
 
-## Figure 3cd
+## Figure 3c
+tbl_3c <- tecan %>% 
+  filter( plate == "CP00001" ) %>%
+  filter( ( gene=="N" & minutes == 30 ) | ( gene=="1a" & minutes==40 ) ) %>%
+  left_join( tblCT ) %>%
+  filter( !is.na(CT) ) %>%
+  mutate( CT = ifelse( CT>40, runif( n(), 43, 47 ), CT ) ) %>%
+  mutate( facet = str_c( gene, " gene, ", minutes, " min" ) )
 
-tecan %>% 
-filter( plate == "CP00001" ) %>%
-filter( ( gene=="N" & minutes == 30 ) | ( gene=="1a" & minutes==40 ) ) %>%
-left_join( tblCT ) %>%
-filter( !is.na(CT) ) %>%
-mutate( CT = ifelse( CT>40, runif( n(), 43, 47 ), CT ) ) %>%
-mutate( facet = str_c( gene, " gene, ", minutes, " min" ) ) %>%
-mutate_at( vars(facet), fct_rev ) %>%
-ggplot +
+tbl_3c %>%
+  filter(gene == "N") %>%
+  ggplot() +
   geom_vline( xintercept = 42, col="darkgray" ) +
   geom_point( aes( x=CT, y=absBlue-absYellow ), size=.7, shape=19, alpha=.6, fill="lightgray" ) +
   scale_x_reverse( breaks = c( 10, 20, 30, 40, 45 ), labels = c( 10, 20, 30, 40, "neg" ) ) +
   xlab( "RT-qPCT (CT value)" ) + 
-  ylab( "RT-LAMP (ΔOD)" ) +
-  facet_wrap( . ~ facet ) -> plot3c
+  ylab( "RT-LAMP (ΔOD)" ) -> plot3c
 
-( ggplot() | plot3b ) / plot3c +
+## Supplementary Figure S3
+tbl_3c %>%
+  filter(gene == "1a") %>%
+  ggplot() +
+  geom_vline( xintercept = 42, col="darkgray" ) +
+  geom_point( aes( x=CT, y=absBlue-absYellow ), size=.7, shape=19, alpha=.6, fill="lightgray" ) +
+  scale_x_reverse( breaks = c( 10, 20, 30, 40, 45 ), labels = c( 10, 20, 30, 40, "neg" ) ) +
+  xlab( "RT-qPCT (CT value)" ) + 
+  ylab( "RT-LAMP (ΔOD)" ) -> plotS3
+
+ggplot() / (plot3b | plot3c) +
   plot_annotation(tag_levels = "a")
 
 ggsave("SVGs/Figure_3.svg", width=20, height=14, units="cm")
 ggsave("Figure_3.png", width=20, height=14, units="cm", dpi=300)
+
+
+plotS3
+
+ggsave("SVGs/Figure_S3.svg", width=10, height=10, units="cm")
+ggsave("Figure_S3.png", width=10, height=10, units="cm", dpi=300)
