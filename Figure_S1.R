0df3d01f1684f6b34b6661f37dcfca6dcfa4ae4f
herbstk <k.herbst@stud.uni-heidelberg.de>
2020-06-04 13:45:31 +0200

    clean-up and complete code Fig S1

--- Figure_S1.R
+++ Figure_S1.R
@@ -10,27 +10,16 @@ filter( plate == "CP00001" ) %>%
 left_join( tblCT_extra ) %>%
 mutate_at( c( "vNCOVE", "vNCOVN" ), 
    ~ ifelse( .=="negativ", Inf, suppressWarnings(as.numeric(.)) ) ) %>%  
-assertr::verify( ( !is.finite(CT) & !is.finite(vNCOVE) ) | abs( CT - vNCOVE ) < .2 ) ->
-qPCRpl1  
+assertr::verify( ( !is.finite(CT) & !is.finite(vNCOVE) ) | abs( CT - vNCOVE ) < .2 ) %>%
+select(-CT) -> qPCRpl1  
 
-read_tsv( "data/tecan_values.tsv" ) %>%
+tecan %>%
   filter( plate == "CP00001", gene == "N", minutes==30 ) %>%
 left_join( qPCRpl1 ) -> tbl
 
-tbl %>%
-mutate_at( c( "vNCOVE", "vNCOVN" ), 
-   ~ ifelse( .=="negativ", runif( n(), 43, 47 ), suppressWarnings(as.numeric(.)) ) ) %>%  
-ggplot() +
-  geom_vline( xintercept = 42, col="lightgrey" ) +
-  geom_point( aes( x = vNCOVE, y = absBlue - absYellow ), 
-    fill = "black", colour = "black", alpha = .6, shape = 21, size = 1.2 ) + 
-  scale_x_reverse( breaks = c( 10, 20, 30, 40, 45 ), labels = c( 10, 20, 30, 40, "neg" ) ) +
-  xlab( "RT-qPCT (CT value)" ) + 
-  ylab( "RT-LAMP (ΔOD)" ) + theme_bw() + theme(panel.grid = element_blank())
-
-tbl %>%
-mutate_at( c( "vNCOVE", "vNCOVN" ), 
-   ~ ifelse( .=="negativ" | is.na(.), runif( n(), 43, 47 ), suppressWarnings(as.numeric(.)) ) ) %>%  
+panel_a <- tbl %>%
+  mutate_at( c( "vNCOVE", "vNCOVN" ), 
+             ~ ifelse( . > 40 | is.na(.), runif( n(), 43, 47 ), suppressWarnings(as.numeric(.)) ) ) %>%
 ggplot() +
   geom_vline( xintercept = 42, col="lightgray" ) +
   geom_point( aes( x = vNCOVN, y = absBlue - absYellow ), 
@@ -38,4 +27,34 @@ ggplot() +
   scale_x_reverse( breaks = c( 10, 20, 30, 40, 45 ), labels = c( 10, 20, 30, 40, "neg" ) ) +
   xlab( "RT-qPCT (CT value for gene 'N')" ) + 
   ylab( "RT-LAMP (ΔOD)" ) + theme_bw() + theme(panel.grid = element_blank())
+panel_a
 
+## TODO @Simon: which one is it?
+tbl_panel_b_diff <- tbl_panel_b %>% mutate(diff = vNCOVN - vNCOVE) %>% summarise(n = n(), mean = mean(diff), se = sd(diff) / sqrt(n))
+tbl_panel_b_diff
+tbl_panel_b_lm <- summary(lm(vNCOVN ~ vNCOVE, tbl_panel_b))
+tbl_panel_b_lm$coefficients[1, 1:2]
+tbl_panel_b_lm$coefficients[1, 2] / tbl_panel_b_diff$n
+
+tbl_panel_b <- tbl %>%
+  filter_at( c( "vNCOVE", "vNCOVN" ), all_vars(!(. > 40 | is.na(.))) )
+panel_b <- tbl_panel_b %>%
+  ggplot() + 
+  geom_point( aes(x=vNCOVE, y=vNCOVN, fill=vNCOVE), 
+              colour = "black", alpha = .8, shape = 21, size = 1.6 ) +
+  coord_fixed() +
+  geom_abline() +
+  geom_abline(slope = 0.8957, intercept = 8.1998, linetype = 2) +
+  geom_abline(slope = 1, intercept = 5.6, linetype = 3) +
+  scale_fill_ct(  name="CT_E" ) +
+  xlab("RT-qPCR (CT value for E gene)" ) +
+  ylab("RT-qPCR (CT value for N gene)" ) 
+panel_b
+
+panel_a + panel_b +
+  plot_annotation(tag_levels = "a") & 
+  theme(plot.tag = element_text(face = "bold"))
+
+# Export figures
+ggsave("SVGs/Figure_S1.svg", width=20, height=10, units="cm")
+ggsave("Figure_S1.png", width=20, height=10, units="cm", dpi=300)
