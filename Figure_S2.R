1b037143c97e366208c054cc92c5add47710c101
herbstk <k.herbst@stud.uni-heidelberg.de>
2020-06-11 14:52:03 +0200

    added missing code for Fig S2

---
+++ Figure_S2.R
@@ -0,0 +1,25 @@
+library( tidyverse )
+library( patchwork )
+source( "misc.R" )
+
+read_tsv( "data/tecan_values.tsv", col_types = "ccliccccdd" ) -> tecan
+read_tsv( "data/plates_with_CTs.tsv" ) -> tblCT
+
+## Supplementary Figure S2
+set.seed(42)
+tecan %>% 
+  filter( plate == "CP00001" ) %>%
+  filter( ( gene=="1a" & minutes==40 ) ) %>%
+  left_join( tblCT ) %>%
+  filter( !is.na(CT) ) %>%
+  mutate( CT = ifelse( CT>40, runif( n(), 43, 47 ), CT ) ) %>%
+  ggplot() +
+  geom_vline( xintercept = 42, col="darkgray" ) +
+  geom_point( aes( x = CT, y = absBlue - absYellow ), fill = "black", colour = "black", alpha = .6, shape = 21, size = 1.2 ) + 
+  scale_x_reverse( breaks = c( 10, 20, 30, 40, 45 ), labels = c( 10, 20, 30, 40, "neg" ) ) +
+  xlab( "RT-qPCT (CT value)" ) + 
+  ylab( "RT-LAMP (ΔOD)" ) -> plotS2
+plotS2
+
+ggsave("SVGs/Figure_S2.svg", width=9, height=7, units="cm")
+ggsave("Figure_S2.png", width=9, height=7, units="cm", dpi=300)
